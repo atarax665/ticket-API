d7136f239a3638aa85a50a7ee2e17cdcc02f8c5c
Atarax665 <manasgmishra@gmail.com>
2022-08-08 00:45:43 +0530

    v1.0.0

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGOURI: "mongodb+srv://atarax:atarax@ticket-api.ub7wvbc.mongodb.net/?retryWrites=true&w=majority",
+  JWT_SECRET: "jkjknjksdkjsdcklmdslkwe"
+}
---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const PORT = process.env.PORT || 3000
+const {MONGOURI} = require('./config/keys')
+const fs = require('fs')
+const path = require('path')
+const morgan = require('morgan')
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger_output.json')
+
+
+mongoose.connect(MONGOURI,{
+ useNewUrlParser: true,
+ useUnifiedTopology: true
+})
+ .then(() => console.log("Connected to MongoDB Successfully"))
+ .catch(err => console.error('Oops, could not connect to mongoDB', err))
+
+ var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+
+ // setup the logger
+ app.use(morgan('combined', { stream: accessLogStream }))
+
+mongoose.set('useFindAndModify', false);
+require('./models/user')
+require('./models/ticket')
+app.use(express.json())
+app.use(require('./routes/auth'))
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
+ app.listen(PORT,()=>{
+     console.log("Server is up at ",PORT)
+ })
